dd76b68be4b3e808418d83d4578cb7619db1dc71
deividy <deividyferreira.n@gmail.com>
2021-11-03 23:25:00 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    return res.send('Olá, NLW')
+} )
+
+app.post('/test-post', (req, res) => {
+    return res.json('Olá, NLW método post')
+} )
+
+app.listen(4000, () => console.log('Server is running on port 4000'))
